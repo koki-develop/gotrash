728a91f38a5ca945b2a95e6e2a0fed2f611cce9a
koki-develop <kou.pg.0131@gmail.com>
2023-03-22 09:28:55 +0900

    implement list command

--- cmd/list.go
+++ cmd/list.go
@@ -1,8 +1,32 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+
+	"github.com/koki-develop/gotrash/internal/db"
+	"github.com/spf13/cobra"
+)
 
 var listCmd = &cobra.Command{
 	Use:     "list",
 	Aliases: []string{"ls"},
+	Args:    cobra.MaximumNArgs(0),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		db, err := db.Open()
+		if err != nil {
+			return err
+		}
+		defer db.Close()
+
+		ts, err := db.List()
+		if err != nil {
+			return err
+		}
+
+		for i, t := range ts {
+			fmt.Printf("%d: %s\n", i, t.Path)
+		}
+
+		return nil
+	},
 }
