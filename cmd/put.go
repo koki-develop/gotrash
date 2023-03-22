0b897d11c3ee6dd29d8770cf8dc6c3166e62b8c5
koki-develop <kou.pg.0131@gmail.com>
2023-03-22 09:28:24 +0900

    implement put command

--- cmd/put.go
+++ cmd/put.go
@@ -1,10 +1,24 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/koki-develop/gotrash/internal/db"
+	"github.com/spf13/cobra"
+)
 
 var putCmd = &cobra.Command{
-	Use: "put",
+	Use:  "put",
+	Args: cobra.MinimumNArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
+		db, err := db.Open()
+		if err != nil {
+			return err
+		}
+		defer db.Close()
+
+		if err := db.Put(args); err != nil {
+			return err
+		}
+
 		return nil
 	},
 }
