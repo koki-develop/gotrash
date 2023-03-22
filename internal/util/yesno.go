c7d0638fbe9f67eaf333cc17d46c39d7d834f982
koki-develop <kou.pg.0131@gmail.com>
2023-03-22 13:05:34 +0900

    implement util.YesNo()

---
+++ internal/util/yesno.go
@@ -0,0 +1,23 @@
+package util
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func YesNo(msg string) bool {
+	sc := bufio.NewScanner(os.Stdin)
+
+	fmt.Printf("%s: ", msg)
+	_ = sc.Scan()
+	yn := sc.Text()
+
+	switch strings.ToLower(yn) {
+	case "y", "yes":
+		return true
+	default:
+		return false
+	}
+}
