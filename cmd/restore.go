05498d24d46f6b08c8702e1be25a43337b1838b9
koki-develop <kou.pg.0131@gmail.com>
2023-03-22 09:29:38 +0900

    implement restore command

--- cmd/restore.go
+++ cmd/restore.go
@@ -1,11 +1,35 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"strconv"
+
+	"github.com/koki-develop/gotrash/internal/db"
+	"github.com/spf13/cobra"
+)
 
 var restoreCmd = &cobra.Command{
 	Use:     "restore",
 	Aliases: []string{"rs"},
 	RunE: func(cmd *cobra.Command, args []string) error {
+		db, err := db.Open()
+		if err != nil {
+			return err
+		}
+		defer db.Close()
+
+		var is []int
+		for _, arg := range args {
+			i, err := strconv.Atoi(arg)
+			if err != nil {
+				return err
+			}
+			is = append(is, i)
+		}
+
+		if err := db.Restore(is); err != nil {
+			return err
+		}
+
 		return nil
 	},
 }
