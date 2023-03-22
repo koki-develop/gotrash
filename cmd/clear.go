eb1d363095bb8907b131a51b5769f695f99b54ca
koki-develop <kou.pg.0131@gmail.com>
2023-03-22 09:29:45 +0900

    implement clear command

--- cmd/clear.go
+++ cmd/clear.go
@@ -1,10 +1,23 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/koki-develop/gotrash/internal/db"
+	"github.com/spf13/cobra"
+)
 
 var clearCmd = &cobra.Command{
 	Use: "clear",
 	RunE: func(cmd *cobra.Command, args []string) error {
+		db, err := db.Open()
+		if err != nil {
+			return err
+		}
+		defer db.Close()
+
+		if err := db.ClearAll(); err != nil {
+			return err
+		}
+
 		return nil
 	},
 }
